7001cc9bb868d794cdb263c7e1bff0b64febfd56
mrprotocoll <lekanvgbg@gmail.com>
2023-06-08 23:35:28 +0100

    Setup react router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from 'Layout';
+import Home from 'pages/Home';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Tour Reservation</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1>Tour Reservation Home page</h1>
+    )
+}
+
+export default Home;
