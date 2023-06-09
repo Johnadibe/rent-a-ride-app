55ddc9dc02c736bf8d81516ee09e487ff4bc0969
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-06-09 08:41:49 +0500

    Build card

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,41 @@
+import React from 'react';
+
 const Home = () => {
+    let data = [
+        {
+            name: "Tour 1",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, sit.",
+            price: 100,
+            image: "https://picsum.photos/200/300"
+        },
+        {
+            name: "Tour 2",
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia sed libero facilis fuga, delectus doloribus?",
+            price: 200,
+            image: "https://picsum.photos/200/300"
+        },
+        {
+            name: "Tour 2",
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia sed libero facilis fuga, delectus doloribus?",
+            price: 200,
+            image: "https://picsum.photos/200/300"
+        }
+    ];
+
     return (
-        <h1>Tour Reservation Home page</h1>
-    )
+        <div className='Row'>
+           {data.map((item, index) => {
+                return (
+                     <div key={index} className='card'>
+                          <img src={item.image} alt={item.name} />
+                          <h3>{item.name}</h3>
+                          <p>Price: {item.price}$</p>
+                          <p className='Des'> {item.description}</p>
+                     </div>
+                );
+           })}
+        </div>
+    );
 }
 
 export default Home;
