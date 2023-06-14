cbe774dc69c52bf6247d335a29636f6c3f8784cb
mrprotocoll <lekanvgbg@gmail.com>
2023-06-14 14:54:07 +0100

    Add login form

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,7 +1,25 @@
-const Login = () => (
-  <div className="login">
-    <h1>Login</h1>
-  </div>
-);
+import { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="auth_container">
+      <form className="auth_form">
+        <h1 className="text-xl font-black">Login</h1>
+        <input type="email" className="form_input" onChange={(element) => setEmail(element.target.value)} placeholder="Email" value={email} required />
+        <input type="password" className="form_input" onChange={(element) => setPassword(element.target.value)} placeholder="Password" value={password} required />
+        <button className="outline_btn" type="submit">Login</button>
+        <div className="alt">
+          Don&apos;t have an account?&nbsp;
+          <a href="/register" className="text-primary">
+            Sign up
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
