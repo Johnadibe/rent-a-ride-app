b11e393e0eaec51b1623474b789d139aee794287
Donmark2k <chukwuemekavp@gmail.com>
2023-06-16 22:45:38 +0100

    Add the correct Navlinks in navlinks.test.js file

--- src/test/navlink.test.js
+++ src/test/navlink.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import NavLinks from '../Components/Navbar/NavLinks';
+import NavLinks from '../components/Navbar/NavLinks';
 
 describe('Tests Navbar component', () => {
   it('Should render Navbar Component', () => {
