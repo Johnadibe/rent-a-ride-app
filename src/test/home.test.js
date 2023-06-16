95261723585112fd35b8ad21ae1ee8c964d299f9
Donmark2k <chukwuemekavp@gmail.com>
2023-06-16 22:22:05 +0100

    Ensure that home.test.js is passing

---
+++ src/test/home.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import Home from '../components/pages/Home';
+
+describe('Tests Navbar component', () => {
+  it('Should render Navbar Component', () => {
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
