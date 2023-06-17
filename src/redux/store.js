d0b02d8149c2eb19de51ccc8597181930eda5393
Donmark2k <chukwuemekavp@gmail.com>
2023-06-17 14:02:46 +0100

    Add reservations to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+import reservationReducer from './reservationSlice';
 
-const store = configureStore({
+const logger = createLogger({
+  // Options for the logger can be passed here
+  // Show only the actions in the console
+  predicate: (getState, action) => action.type !== 'SOME_ACTION',
+  // Collapse the logs by default
+  collapsed: true,
+});
+
+export const store = configureStore({
   reducer: {
+    reservations: reservationReducer,
+    // rockets: rocketReducer,# Another reducer can be here
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
