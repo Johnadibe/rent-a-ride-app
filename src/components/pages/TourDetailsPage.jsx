623eaf2387879487b7400a11f3ac5821da9a7147
John Adibe <johnadibe123@gmail.com>
2023-06-19 18:38:44 -0800

    Update Tour Details page

--- src/components/pages/TourDetailsPage.jsx
+++ src/components/pages/TourDetailsPage.jsx
@@ -3,9 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import { BiRightArrow, BiLeftArrow } from 'react-icons/bi';
-import { AiOutlineRight } from 'react-icons/ai';
 import { SlSettings } from 'react-icons/sl';
-import pic from '../../assets/canva.png';
 import Loader from '../Loader/Loader';
 import { fetchTours } from '../../redux/tours/tours';
 
@@ -26,11 +24,11 @@ const TourDetailsPage = () => {
       {loading && <Loader />}
       {tourss && (
         <div className="flex flex-col items-center md:justify-start justify-center w-full md:flex-row grow h-full lg:pt-20 lg:pb-10">
-          <div className="grow  md:w-5/6 flex items-center justify-center md:px-10 rounded-full aspect-square">
+          <div className="grow flex items-center justify-center">
             <img
               src={`http://localhost:3000${tourss?.image_url}`}
               alt={decodeURIComponent(tourss?.name)}
-              className="object-cover block rounded-full m-4 aspect-square w-[100%] md:ml-[40%]"
+              className="block m-4 aspect-square md:ml-[40%]"
             />
           </div>
           <div className="flex flex-col w-full items-start md:items-end  md:mr-10 py-10 px-10 lg:px-0 text-center">
@@ -38,6 +36,9 @@ const TourDetailsPage = () => {
               <h1 className="md:text-right mb-4 text-3xl font-semibold text-slate-800">
                 {decodeURIComponent(tourss?.name)}
               </h1>
+              <h3 className="md:text-right mb-4 text-lg font-semibold text-slate-800">
+                {decodeURIComponent(tourss?.city)}
+              </h3>
               <p className=" mb-10  md:text-right text-gray-500 text-sm xl:w-1/2 lg:w-1/2">
                 {decodeURIComponent(tourss?.des)}
               </p>
@@ -59,12 +60,17 @@ const TourDetailsPage = () => {
                   </li>
                 </ul>
               </div>
-              <p className="flex items-center gap-2 mt-1">
-                DISCOVER MORE MODELS
-                {' '}
-                <AiOutlineRight className="text-yellow-500" />
-              </p>
-              <img src={pic} alt="canva" />
+              <h5 className="font-bold mt-1 mb-3">
+                EXPLORE TOUR EVENT PLACE
+              </h5>
+              <iframe
+                width="300"
+                height="200"
+                src={`${tourss.video}`}
+                title={decodeURIComponent(tourss?.name)}
+                allow="autoplay"
+                allowfullscreen
+              />
               <div className="my-6 flex justify-center">
                 <button
                   type="button"
@@ -83,7 +89,7 @@ const TourDetailsPage = () => {
       )}
       <div
         className="fixed text-[1.8rem]
-                            bottom-14 left-60 z-10 bg-[#98bd2a] text-white
+                            bottom-[2rem] left-[20rem] z-10 bg-[#98bd2a] text-white
                             rounded-full p-3 cursor-pointer
                             hidden md:block
                             "
