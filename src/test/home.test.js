3db83e5734b8fc3a5e22d1e1335fa9e159104acc
John Adibe <johnadibe123@gmail.com>
2023-06-20 14:01:40 -0800

    Update Home component test

--- src/test/home.test.js
+++ src/test/home.test.js
@@ -4,17 +4,23 @@ import renderer from 'react-test-renderer';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from "react-redux";
+
+import store from "redux/store";
+
 import Home from '../components/pages/Home';
 
 describe('Tests Home component', () => {
   it('Should render Navbar Component', () => {
     const tree = renderer
       .create(
+        <Provider store={store}>
         <BrowserRouter>
           <Home />
-        </BrowserRouter>,
+        </BrowserRouter>
+        </Provider>,
       )
       .toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot('Tests Home component Should render Navbar Component');
   });
 });
