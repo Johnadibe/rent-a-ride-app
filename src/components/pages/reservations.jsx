b7d47ba52ec912d03ffcc7b88dab63aa3e3ebe8f
John Adibe <johnadibe123@gmail.com>
2023-06-20 14:59:22 -0800

    Updated codes"

--- src/components/pages/reservations.jsx
+++ src/components/pages/reservations.jsx
@@ -1,9 +1,6 @@
-/* eslint-disable no-alert */
-/* eslint-disable import/extensions */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteReservation, fetchReservations } from '../../redux/reservationSlice';
-import { fetchTours } from '../../redux/tours/tours';
 
 const Reservations = () => {
   const dispatch = useDispatch();
@@ -11,16 +8,15 @@ const Reservations = () => {
 
   useEffect(() => {
     dispatch(fetchReservations());
-    dispatch(fetchTours());
   }, [dispatch]);
 
   const getTourName = (id) => {
-    const tour = reservations.find((yourTour) => yourTour.id === id);
+    const tour = tours.find((yourTour) => yourTour.id === id);
     return tour ? tour.name : '';
   };
 
   const getTourImage = (id) => {
-    const tour = reservations.find((yourTour) => yourTour.id === id);
+    const tour = tours.find((yourTour) => yourTour.id === id);
     return tour ? tour.img_url : '';
   };
 
@@ -51,14 +47,13 @@ const Reservations = () => {
             key={reservation.id}
           >
             <img
-              src={getTourImage(reservation.tour_id)}
-              alt={getTourName(reservation.tour_id)}
+
               className="h-44 w-96 object-cover rounded-t-lg"
             />
             <div className="p-6 flex-grow flex flex-col justify-between">
               <div>
                 <p className="text-lg font-bold mb-2 text-gray-800">
-                  {getTourName(reservation.tour_id)}
+                  {getMotorcycleName(reservation.motorcycle_id)}
                 </p>
                 <div className="flex justify-between items-center mb-2">
                   <span className="text-gray-700 font-bold">City:</span>
@@ -77,7 +72,6 @@ const Reservations = () => {
               </div>
               <div className="flex justify-end">
                 <button
-                  type="button"
                   className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mt-2"
                   onClick={() => handleDeleteReservation(reservation.id)}
                 >
