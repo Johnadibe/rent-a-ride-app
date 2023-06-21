0d75b8349caf1d50236820d6ccc701864685ecd6
John Adibe <johnadibe123@gmail.com>
2023-06-21 12:50:46 -0800

    Add margin right to Login button

--- src/components/Lognin/out/log.jsx
+++ src/components/Lognin/out/log.jsx
@@ -18,7 +18,7 @@ const Log = () => {
 
   if (isLoggedIn) {
     return (
-      <div className="md:mr-12">
+      <div className=" mr-4 md:mr-12">
         <button className="relative inline-flex items-center justify-center p-0.5 mb-2 md:mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800" type="submit" onClick={handleLogout}>
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
             Log Out
@@ -28,7 +28,7 @@ const Log = () => {
     );
   }
   return (
-    <div className="md:mr-12">
+    <div className="mr-4 md:mr-12">
       <button className="relative inline-flex items-center justify-center p-0.5 mb-2 md:mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800" type="submit">
         <a href="http:/login" className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
           Login
