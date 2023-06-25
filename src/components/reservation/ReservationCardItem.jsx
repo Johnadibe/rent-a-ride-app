6f235646d6cbac5d897360c300a70e603c6c12dd
mrprotocoll <lekanvgbg@gmail.com>
2023-06-25 15:32:30 +0100

    validate props for reservation card items

--- src/components/reservation/ReservationCardItem.jsx
+++ src/components/reservation/ReservationCardItem.jsx
@@ -1,3 +1,5 @@
+import { PropTypes } from 'prop-types';
+
 const ReservationCardItem = ({ name, value }) => (
   <div className="flex justify-between items-center mb-2">
     <span className="text-gray-700 font-bold">
@@ -7,5 +9,10 @@ const ReservationCardItem = ({ name, value }) => (
     <span className="text-gray-700">{value}</span>
   </div>
 );
+
+ReservationCardItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
 
 export default ReservationCardItem;
